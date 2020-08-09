a0c526cbd0fb85b9c56d90953b9f0d27e14578b5
German Cruz <cruzgerman216@yahoo.com>
2020-08-09 05:57:24 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
-     if song.artist 
+     if song.artist
       link_to "#{song.artist.name}", artist_path(song.artist)
    else
        edit_song_path(song)
     end
-
   end
 end
